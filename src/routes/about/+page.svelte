af4bef30ae9c4a05ec9028145adb137c3a08e831
Daniel Baron <daniel@barooney.com>
2024-01-31 23:49:16 +0100

    Added TailwindCSS

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,7 +4,7 @@
 </svelte:head>
 
 <div class="text-column">
-	<h1>About this app</h1>
+	<h1 class="text-blue-500">About this app</h1>
 
 	<p>
 		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
