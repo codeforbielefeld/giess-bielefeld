39b99f8285d0f281d057ea726c15cd2f8f2f4e7a
Daniel Baron <daniel@barooney.com>
2024-08-29 21:21:04 +0200

    Updated supabase migrations to resemble similar structure for the new trees geojson file. Updated import to load new trees geojson file.

--- preparation/import.py
+++ preparation/import.py
@@ -6,8 +6,9 @@ from supabase import create_client, Client
 from shapely import wkt, Point
 import pyproj
 import sys
+from tqdm import tqdm
 
-load_dotenv("../frontend/.env")
+load_dotenv("../.env")
 
 url: str = os.environ.get("VITE_SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_SERVICE_ROLE_KEY")
@@ -27,47 +28,43 @@ def unproject(x, y):
     return projection(x, y, inverse=True)
 
 
-def create_insert_data(feature):
-    geo_coordinates = feature["geometry"]["coordinates"]
-
-    point = unproject(geo_coordinates[0], geo_coordinates[1])
-    point_data = "POINT(" + str(point[0]) + " " + str(point[1]) + ")"
-    if "inf" in point_data:
-        print("SKIPPING tree with coordinates", geo_coordinates[0], geo_coordinates[1])
-        return None
-
+def create_insert_data(feature, provider_id):
     return {
+        "provider_id": provider_id,
+        "source_id": feature["properties"]["pitID"],
         "location": feature["properties"]["Standort_N"],
         "location_addition": feature["properties"]["Zusatz"],
         "current_number": feature["properties"]["laufende_n"],
         "chopped": feature["properties"]["gefaellt"],
         "trunk_diameter": feature["properties"]["Stammdurch"],
+        "trunk_circumference": feature["properties"]["Stammumfan"],
         "crown_diameter": feature["properties"]["Kronendurc"],
         "height": feature["properties"]["Baumhoehe_"],
-        "trunk_circumference": feature["properties"]["Stammumfan"],
         "tree_group": feature["properties"]["Baumgruppe"],
-        "district_reference": feature["properties"]["ref_distri"],
         "district_number": feature["properties"]["Bezirk_Nr"],
         "district_name": feature["properties"]["Bezirk_Bez"],
-        "outside_reference": feature["properties"]["ref_outsid"],
         "object_number": feature["properties"]["Objekt_Nr"],
         "object_name": feature["properties"]["Objekt_Bez"],
-        "tree_type_reference": feature["properties"]["ref_Baumar"],
         "tree_type_botanic": feature["properties"]["Baumart_bo"],
         "tree_type_german": feature["properties"]["Baumart_de"],
         "tree_type_short": feature["properties"]["Baumart_ku"],
-        "outdoor_reference": feature["properties"]["ref_outdoo"],
         "care_number": feature["properties"]["PflE_Art_N"],
         "care_type": feature["properties"]["PflE_Art_B"],
         "trunk_radius": feature["properties"]["Stammradiu"],
         "crown_radius": feature["properties"]["Kronenradi"],
-        "geocoordinates": point_data
+        "geocoordinates": "POINT(" + str(feature["geometry"]["coordinates"][0]) + " " + str(feature["geometry"]["coordinates"][1]) + ")"
     }
 
 
 with open(path_to_geojson) as f:
     data = json.load(f)
 
-    rows = [create_insert_data(feature) for feature in tqdm(data["features"])]
+    city = supabase.table("providers").insert({
+        "name": "Stadt Bielefeld",
+    }).execute()
+    city_uuid = city.data[0]["uuid"]
+
+    rows = [create_insert_data(feature, city_uuid) for feature in data["features"]]
     rows = [row for row in rows if row is not None]
-    supabase.table("trees").insert(rows).execute()
+    for row in tqdm(rows):
+        supabase.table("trees").insert(row).execute()
--- supabase/migrations/20240316110616_create_nearby_trees_function.sql
+++ supabase/migrations/20240316110616_create_nearby_trees_function.sql
@@ -1,8 +1,8 @@
 CREATE OR REPLACE FUNCTION nearby_trees(lat float, long float)
-    RETURNS TABLE (id public.trees.id%TYPE, tree_type_german public.trees.tree_type_german%TYPE, lat float, long float, dist_meters float)
+    RETURNS TABLE (uuid public.trees.uuid%TYPE, tree_type_german public.trees.tree_type_german%TYPE, lat float, long float, dist_meters float)
     LANGUAGE sql
 AS $$
-    SELECT id, tree_type_german, ST_Y(geocoordinates::geometry) AS lat, ST_X(geocoordinates::geometry) as long, ST_Distance(geocoordinates, st_point(long, lat)::geography) AS dist_meters
+    SELECT uuid, tree_type_german, ST_Y(geocoordinates::geometry) AS lat, ST_X(geocoordinates::geometry) as long, ST_Distance(geocoordinates, st_point(long, lat)::geography) AS dist_meters
     FROM public.trees
     ORDER BY geocoordinates <-> st_point(long, lat)::geography;
 $$;
